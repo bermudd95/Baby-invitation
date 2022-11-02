a1c10e7f48be7eee2c2e92ae68631977fdc0a9e9
Danny Bermudez <danny@Dannys-MacBook-Pro.local>
2022-11-01 23:03:57 -0700

    Adding source code

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import Service from './Server/Service';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card-container">
+      <img className='background' src='https://images.unsplash.com/photo-1546074176-abecd33d2b53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'/>
+      <div className="title-container">
+        <div className="title">
+          <h1>
+            You are invited!!
+          </h1>
+          <h3>
+            Come join us in Wendy's and Danny's baby shower!
+          </h3>
+        </div>
+      </div>
+      <div className="body-content">
+        <ul>
+          <li>
+            <p>
+              When:
+            </p>
+            <p>
+              January 28th, 2023
+            </p>
+          </li>
+          <li>
+            <p>
+              Where:
+            </p>
+            <p>
+              Lower Scholls Canyon Park
+            </p>
+          </li>
+          <li>
+            <p>
+              Directions:
+            </p>
+            <p>
+              2849 E. Glenoaks Blvd
+              Glendale, CA 91206
+            </p>
+          </li>
+        </ul>
+        <h2>
+          Important times:
+        </h2>
+        <ul>
+          <li>
+            <p>
+              Party starts:
+            </p>
+            <p>
+              3:00pm
+            </p>
+          </li>
+          <li>
+            <p>
+              Food is served:
+            </p>
+            <p>
+              3:45pm
+            </p>
+          </li>
+          <li>
+            <p>
+              Games:
+            </p>
+            <p>
+              5:50pm
+            </p>
+          </li>
+          <li>
+            <p>
+              Present opening:
+            </p>
+            <p>
+              7:30pm
+            </p>
+          </li>
+          <li>
+            <p>
+              Party ends:
+            </p>
+            <p>
+              9:00pm
+            </p>
+          </li>
+        </ul>
+      </div>
+      <Service/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Server/Service.jsx
@@ -0,0 +1,50 @@
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser'
+import { TextField }  from '@mui/material';
+import { Button } from '@mui/material';
+
+
+    const Service = () => {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_u2f5wlu', 'template_aqibbfi', form.current, 'dkSCkT9G1nrflBiqD')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+    return (
+        <form ref={form} name="form" onSubmit={sendEmail}>
+        <div className="form-content">
+          <div className="instructions-container">
+            <h4>
+              Please fill out this form to confirm attendance.
+            </h4>
+          </div>
+          <div className="questions-container">
+            <h4>
+              Name:
+            </h4>
+            <TextField id="outlined-basic" label="Full Name" variant="outlined" name="user_name" />
+            <h4>
+              Party size:
+            </h4>
+            <TextField id="outlined-basic" label="Party size" variant="outlined" name="party_size" />
+            <h4>
+              Attending:
+            </h4>
+            <TextField id="outlined-basic" label="Yes or No" variant="outlined" name="attending" />
+          </div>
+          <div className='button-container'>
+              <Button variant="contained" type="submit" value="send">Submit</Button>
+          </div>
+        </div>
+      </form>
+    )
+}
+
+export default Service;
