03019de3a6c1a6a5495fdbefaa63cd4bf6f302f9
Danny Bermudez <danny@Dannys-MacBook-Pro.local>
2022-11-27 20:07:07 -0800

    update email

--- src/Server/Service.jsx
+++ src/Server/Service.jsx
@@ -1,20 +1,26 @@
-import React, {useRef} from 'react'
+import React, {useRef,useState} from 'react'
 import emailjs from '@emailjs/browser'
 import { TextField }  from '@mui/material';
 import { Button } from '@mui/material';
 
 
     const Service = () => {
-    const form = useRef();
+      const form = useRef();
+      const [name, setName] = useState('');
+      const [partySize, setPartySize] = useState('');
+      const [attending, setAttending] = useState('');
   
     const sendEmail = (e) => {
-      
+      e.preventDefault();
       emailjs.sendForm('service_u2f5wlu', 'template_aqibbfi', form.current, 'dkSCkT9G1nrflBiqD')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
             console.log(error.text);
         });
+        setName('');
+        setPartySize('');
+        setAttending('');
     };
     return (
         <form ref={form} name="form" onSubmit={sendEmail}>
@@ -28,15 +34,15 @@ import { Button } from '@mui/material';
             <h4>
               Name:
             </h4>
-            <TextField id="outlined-basic" label="Full Name" variant="outlined" name="user_name" />
+            <TextField id="outlined-basic" label="Full Name" variant="outlined" name="user_name" onChange={e => setName(e.target.value)} value={name} />
             <h4>
               Party size:
             </h4>
-            <TextField id="outlined-basic" label="Party size" variant="outlined" name="party_size" />
+            <TextField id="outlined-basic" label="Party size" variant="outlined" name="party_size" onChange={e => setPartySize(e.target.value)} value={partySize}/>
             <h4>
               Attending:
             </h4>
-            <TextField id="outlined-basic" label="Yes or No" variant="outlined" name="attending" />
+            <TextField id="outlined-basic" label="Yes or No" variant="outlined" name="attending" onChange={e => setAttending(e.target.value)} value={attending} />
           </div>
           <div className='button-container'>
               <Button variant="contained" type="submit" value="send">Submit</Button>
